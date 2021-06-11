98a5d3b14a0a5dea7ee8446c0bf9e6476f967137
Erick <erickzanardoo@gmail.com>
2021-06-11 20:21:34 -0300

    Adding MacOS native files (#2)
    * Adding native files
    
    * fixing lua compiling
    
    * fixing macos build
    
    * get building using official lua pod (#3)
    
    Co-authored-by: Christopher Fujino <christopherfujino@gmail.com>

---
+++ plugin/example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,12 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import lua_flame
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FireMoonPlugin.register(with: registry.registrar(forPlugin: "FireMoonPlugin"))
+}
